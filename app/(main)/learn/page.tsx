5333138e97b7c204ca4120ea4366d33697722f04
rizchi17 <rizchi17ub@gmail.com>
2024-03-23 16:53:28 +0900

    ハードコーディングしていた部分の置き換え

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -16,14 +16,14 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         ></UserProgress>
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish"></Header>
+        <Header title={userProgress.activeCourse.title}></Header>
       </FeedWrapper>
     </div>
   );
