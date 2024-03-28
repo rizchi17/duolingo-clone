3c905aff5bccaebf77b901c887a8624b8662d781
rizchi17 <rizchi17ub@gmail.com>
2024-03-28 23:56:05 +0900

    challengeのtypeに合わせて表示をかえる

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -4,6 +4,7 @@ import { challengeOptions, challenges } from "@/db/schema";
 import { headers } from "next/headers";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
 
 type Props = {
   initialPercentage: number;
@@ -25,6 +26,20 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed,
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning?"
+      : challenge.question;
   return (
     <>
       <Header
@@ -32,6 +47,21 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       ></Header>
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {/*TODO: change back to type ASSIST */}
+              {challenge.type === "SELECT" && (
+                <QuestionBubble question={challenge.question}></QuestionBubble>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
