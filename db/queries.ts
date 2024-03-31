a4c1c4bba8b0bc4f0217050dabf8906478982eca
rizchi17 <rizchi17ub@gmail.com>
2024-03-31 23:27:04 +0900

    getUserSubscription

--- db/queries.ts
+++ db/queries.ts
@@ -7,6 +7,7 @@ import {
   lessons,
   units,
   userProgress,
+  userSubscription,
 } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
@@ -198,4 +199,26 @@ export const getLessonPercentage = cache(async () => {
   );
 
   return percentage;
+});
+
+const DAY_IN_MS = 86_400_000;
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userSubscription.findFirst({
+    where: eq(userSubscription.userId, userId),
+  });
+  if (!data) return null;
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return {
+    ...data,
+    isActive: !!isActive,
+  };
 });
